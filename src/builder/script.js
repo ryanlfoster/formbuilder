bcc849f48e4fe7d02c58413f6646dda372f300e6
Tjebbe Lievens <tjebbelievens@hotmail.com>
2014-05-06 14:34:31 +0200

    mobile browser bug fix

--- src/builder/script.js
+++ src/builder/script.js
@@ -53,6 +53,7 @@
  					formRev:'',
  					basicHTML:'<!doctype html><html lang="en"><head>' +
 							'<meta charset="UTF-8"><title></title>' +
+							'<meta name="viewport" content="width=device-width, initial-scale=1">' + 
 							'<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">' +
 							'<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">' +
 							'<script src="https://code.jquery.com/jquery-2.1.0.js"></script>' +
